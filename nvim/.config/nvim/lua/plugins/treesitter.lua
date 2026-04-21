fd6a88f54fbe3e5ac5832f0f40f5bcc956c77c3d
crmostyn <caleb.mostyn@ciholas.com>
2026-04-21 10:18:36 -0500

    Fixing Indentation in C++

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -18,9 +18,9 @@ return {
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'cpp' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'cpp' } },
     },
     -- config = function(_, opts)
     --   require('nvim-treesitter.configs').setup(opts)
